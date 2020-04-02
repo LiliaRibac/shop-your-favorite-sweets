230bf748ce3ba7806dcb9e5e895a89a5324a1254
LiliaRibac <ribac.lilia@gmai.com>
2020-04-02 07:33:20 -0700

    show the totals amount

--- js/app.js
+++ js/app.js
@@ -36,10 +36,55 @@
 
         let finalPrice = price.slice(1).trim();
         item.price = finalPrice;
-        console.log(finalPrice);
-        console.log(item);
-        // console.log(price)
+
+        const cartItem = document.createElement('div');
+        cartItem.classList.add(
+          'cart-item',
+          'd-flex',
+          'justify-content-between',
+          'text-capitalize',
+          'my-3'
+        );
+        cartItem.innerHTML = ` 
+        <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+        <div class="item-text">
+
+          <p id="cart-item-title" class="font-weight-bold mb-0"> ${item.name}</p>
+          <span>$</span>
+          <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+        </div>
+        <a href="#" id='cart-item-remove' class="cart-item-remove">
+          <i class="fas fa-trash"></i>
+        </a>
+     `;
+
+// select cart
+const cart = document.getElementById("cart");
+const total = document.querySelector(".cart-total-container")
+
+cart.insertBefore(cartItem,total)
+alert("item added to the cart")
+showTotals()
       }
     });
   });
+
+  // show totals
+  function showTotals(){
+    const total =[];
+    const items = document.querySelectorAll(".cart-item-price")
+
+    items.forEach(function(item){
+      total.push(parseFloat(item.textContent))
+    })
+    console.log(total)
+    const totalMoney = total.reduce(function(total, item){
+      total+=item;
+      return total
+    },0);
+
+    const finalMoney = totalMoney.toFixed(2)
+    console.log(finalMoney)
+  }
+
 })();
