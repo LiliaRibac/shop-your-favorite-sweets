2dd971e472efd22d5337587150fea28d81aecfad
LiliaRibac <ribac.lilia@gmai.com>
2020-04-02 06:47:22 -0700

    add funct for a cart

---
+++ js/app.js
@@ -0,0 +1,45 @@
+//  show cart
+
+(function() {
+  const cartInfo = document.getElementById('cart-info');
+  const cart = document.getElementById('cart');
+
+  cartInfo.addEventListener('click', function() {
+    cart.classList.toggle('show-cart');
+  });
+})();
+// add items to the cart
+
+(function() {
+  const cartBtn = document.querySelectorAll('.store-item-icon');
+
+  cartBtn.forEach(function(btn) {
+    btn.addEventListener('click', function(event) {
+      if (event.target.parentElement.classList.contains('store-item-icon')) {
+        let fullPath = event.target.parentElement.previousElementSibling.src;
+
+        let pos = fullPath.indexOf('img') + 3;
+        let partPath = fullPath.slice(pos);
+        console.log(partPath);
+
+        const item = {};
+        item.img = `img-cart${partPath}`;
+
+        let name =
+          event.target.parentElement.parentElement.nextElementSibling
+            .children[0].children[0].textContent;
+        item.name = name;
+
+        let price =
+          event.target.parentElement.parentElement.nextElementSibling
+            .children[0].children[1].textContent;
+
+        let finalPrice = price.slice(1).trim();
+        item.price = finalPrice;
+        console.log(finalPrice);
+        console.log(item);
+        // console.log(price)
+      }
+    });
+  });
+})();
